30cdbd92ed6c048e6dce40394039ecbdb12d2023
rktdnjs <rktdnjs@naver.com>
2023-01-20 01:11:37 +0900

    6단계 2941

--- 3단계/2438.py
+++ 3단계/2438.py
@@ -1,5 +1,5 @@
 numbers = int(input())
 
 for i in range(1, numbers+1):
-    print(" " * (numbers-i), end="")
+    print(" " * (numbers-i), end="") # end를 쓰면, 그 뒤의 출력값을 이어서 출력(줄바꿈을 하지 않는다!)
     print("*" * i)
