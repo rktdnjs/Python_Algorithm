d129b4076794e753d43f9a3ecfba2f6d5dd8815f
rktdnjs <rktdnjs@naver.com>
2023-03-20 22:47:25 +0900

    10773 - 덱 자료구조

---
+++ 스택&큐/10773.py
@@ -0,0 +1,20 @@
+# 10773 : 제로
+# https://www.acmicpc.net/problem/10773
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+
+List = deque([])
+
+for i in range(N):
+    number = int(input())
+    if number == 0:
+        List.pop()
+    else:
+        List.append(number)
+
+print(sum(List))
