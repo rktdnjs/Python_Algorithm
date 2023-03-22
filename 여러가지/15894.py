03463960569174f03902c8ff5c13e5ea15b7d8c9
rktdnjs <rktdnjs@naver.com>
2023-03-23 01:25:47 +0900

    1234, 15894

---
+++ 여러가지/15894.py
@@ -0,0 +1,14 @@
+# 15894 : 수학은 체육과목입니다
+# https://www.acmicpc.net/problem/15894
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+print(4*N)
+
+# 4 3 + 1
+# 8 3 + 1.5 + 1.5 + 2
+# 12  3 + 1.5 + 1.5 + 1.5 + 1.5 + 3
