ff2218e26c839b1ae8fd89bcdb689d7ba4f161a8
rktdnjs <rktdnjs@naver.com>
2023-04-11 23:17:23 +0900

    24263 - 시간복잡도 문제 2

---
+++ 10단계(시간복잡도)/24263.py
@@ -0,0 +1,9 @@
+# 24263 : 알고리즘 수업 - 알고리즘의 수행 시간2
+# https://www.acmicpc.net/problem/24263
+
+N = int(input())
+
+# 해당 문제의 MenOfPassion 알고리즘은 입력에 따라 N번의 연산을 수행한다.
+# 그렇기 때문에 이 경우 시간 복잡도는 N. O(N)이 소요된다.
+print(N)
+print(1)
