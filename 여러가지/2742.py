d175c8033eff3326ef32c55d1d69d7a401c64193
rktdnjs <rktdnjs@naver.com>
2023-05-07 23:32:33 +0900

    Update Algorithm Folders

--- 여러가지/2742.py
+++ 여러가지/2742.py
@@ -1,8 +1,14 @@
-# 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
-# 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+# 2742 : 기찍 N
+# https://www.acmicpc.net/problem/2742
 
-Input = int(input())
+import sys
 
+N = int(sys.stdin.readline().strip())
+
+for i in range(N, 0, -1):
+    print(i)
+
+"""
 for i in reversed(range(Input)):
     print(i + 1)
 
@@ -10,3 +16,4 @@ for i in reversed(range(Input)):
 # range()는 파라미터 start, step은 생략 가능하며 생략할 시 0, 1로 주어진다.
 # range([start] stop [step])의 형태로 구성됨
 # 만약 step을 -1로 주게되면, 거꾸로 출력됨
+"""
--- 카테캠대비/DP/1463.py
+++
@@ -1,4 +0,0 @@
-# 1463 : 1로 만들기
-# https://www.acmicpc.net/problem/1463
-# 동적 계획법.
-
