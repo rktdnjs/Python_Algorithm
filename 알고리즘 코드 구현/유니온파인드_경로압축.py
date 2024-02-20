fc8e53685d5286ae4125bc952ad4149057b4fe33
rktdnjs <rktdnjs@naver.com>
2024-02-20 17:18:44 +0900

    유니온파인드_경로압축

---
+++ 알고리즘 코드 구현/유니온파인드_경로압축.py
@@ -0,0 +1,53 @@
+# 특정 원소가 속한 집합 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면 루트 노드를 찾을 때까지 재귀 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b: # a의 부모 노드보다 b의 부모 노드가 크면
+        parent[b] = a # b의 부모 노드를 a의 부모 노드로 연결
+    else: # a의 부모 노드가 b의 부모 노드보다 크면
+        parent[a] = b # a의 부모 노드를 b의 부모 노드로 연결
+
+# 노드의 개수 & 간선(union 연산)의 개수 입력
+v, e = map(int, input().split())
+parent = [0] * (v + 1) # 부모 테이블 초기화
+
+# 부모 테이블상에서 부모를 자기 자신으로 초기화
+for i in range(1, v + 1):
+    parent[i] = i
+
+# union 연산을 각각 수행
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+
+# 모든 유니온 연산이 끝난뒤 각 원소가 속한 집합 출력
+print('각 원소가 속한 집합: ', end='')
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end=' ')
+
+print() # 줄바꿈
+
+# 부모 테이블 내용 출력
+print('부모 테이블: ', end='')
+for i in range(1, v + 1):
+    print(parent[i], end=' ')
+
+
+"""
+예시 입출력
+6 4
+1 4
+2 3
+2 4
+5 6
+각 원소가 속한 집합: 1 1 1 1 5 5 
+부모 테이블: 1 1 1 1 5 5
+"""
